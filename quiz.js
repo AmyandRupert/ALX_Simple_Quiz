42ad6c63dff6e257e78e5a2929fce88f245924bd
AmyandRupert <amyleroux432@gmail.com>
2025-08-03 19:47:35 +0200

    Updated styles.css index.html quiz.js

---
+++ quiz.js
@@ -0,0 +1,10 @@
+function checkAnswer() {
+    const correctAnswer = "4";
+const idValue = document.querySelector(name= 'quiz');
+const userAnswer = document.getElementById(feedback).value;}
+if (userAnswer === correctAnswer) {
+    idValue.textContent = "Correct! Well done.";}
+else {
+    idValue.textContent = "That's incorrect. Try again!";}
+const submitanswer = document.getElementById("submit-answer");
+submitanswer.addEventListener (click, checkAnswer);
